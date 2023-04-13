407001a17b45f883fb1dc53daccbff150026f190
Jakub Pavlik <severus@post.cz>
2023-04-13 19:11:20 +0200

    psalter_clean: drop date

--- bin/psalter_clean.rb
+++ bin/psalter_clean.rb
@@ -6,7 +6,7 @@ require_relative '../lib/constants'
 ModifyValues
   .new do |row|
   if row['cycle'] == Cycle::PSALTER
-    %w(month day day_title).each {|col| row[col] = nil }
+    %w(month day date day_title).each {|col| row[col] = nil }
   end
 end
   .run(ARGF)
